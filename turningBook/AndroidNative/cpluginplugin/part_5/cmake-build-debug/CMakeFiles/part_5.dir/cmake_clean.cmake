b1cb361ad8abaeb0a02fd9fbfb4d07df6fcc0292
“renyushuang” <renyushuang501@163.com>
2020-05-24 19:22:45 +0800

    change: c++更换目录

---
+++ turningBook/AndroidNative/cpluginplugin/part_5/cmake-build-debug/CMakeFiles/part_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/part_5.dir/main.cpp.o"
+  "part_5"
+  "part_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/part_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
