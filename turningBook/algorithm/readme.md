06606bf8b134db646defd320121ff9d9c73ed250
“renyushuang” <renyushuang501@163.com>
2020-05-31 23:09:19 +0800

    change： 增加算法的部分

---
+++ turningBook/algorithm/readme.md
@@ -0,0 +1,4 @@
+# 算法
+## 数组(array)
+- 移除重复从有序数组(RemoveDuplicatesFromSortedArray.class)
+- 
---
+++ turningBook/algorithm/src/array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,22 @@
+package array;
+
+/**
+ * 移除重复从有序数组
+ * <p>
+ * 给出一个有序数组，删除重复项，使每个元素只出现一次，并返回新的长度
+ * <p>
+ * 不要为另一个数组分配额外的空间，你必须使用恒定的空间
+ * <p>
+ * 例如给你A = [1,1,2],那么应该返回 length = 2,A = [1,2]
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        System.out.println("开始第一个编程");
+        int[] A = {1, 1, 2};
+
+
+
+
+    }
+}
